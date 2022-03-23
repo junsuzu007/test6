d24bcb625405383f137aadd5181973e018e1ff68
evast55 <r.tsujii@evast.jp>
2022-03-23 16:59:46 +0900

    edit page1VC , set  identifier page1

--- t6/page1VC.swift
+++ t6/page1VC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class page1VC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
 
